bda567b39f6c341adb02bc9b277dec1024dd0a63
Corey Matthew Capetillo <corey.capetillo@gmail.com>
2024-04-20 12:24:49 -0700

    Move CharacterKey to ID list

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@ import BaseClient from '@/BaseClient';
 
 export { Characters, CharacterIDs, Resources, Stats, Languages, Properties, Paths } from '@/enum';
 
+export { AchievementsClient } from '@clients/AchievementsClient';
+export { AvatarsClient } from '@clients/AvatarsClient';
+export { CharacterPromotionsClient } from '@clients/CharacterPromotionsClient';
 export { CharacterRanksClient } from '@clients/CharacterRanksClient';
 export { CharactersClient } from '@clients/CharactersClient';
+export { CharacterSkillsClient } from '@clients/CharacterSkillsClient';
+export { CharacterSkillTreesClient } from '@clients/CharacterSkillTreesClient';
+export { ItemsClient } from '@clients/ItemsClient';
 
 export type {
+  Achievement,
   Avatar,
   CharacterPromotion,
   CharacterPromotionStat,
